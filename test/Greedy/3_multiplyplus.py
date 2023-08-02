3746631f95287b5bd7af58931794b1906a5fcda7
soy53 <116326867+soy53@users.noreply.github.com>
2023-08-02 09:38:46 +0000

    2 answers

--- test/Greedy/3_multiplyplus.py
+++ test/Greedy/3_multiplyplus.py
@@ -1,16 +1,16 @@
 # 곱하기 혹은 더하기
 # 풀이 방법 1(by me)
-# n = input()
+n = input()
 
-# result = 0
+result = 0
 
-# for i in range(len(n)):
-#   if result == 0 or result == 1:
-#     result += int(n[i])
-#   else:
-#     result *= int(n[i])
+for i in range(len(n)):
+  if result == 0 or result == 1:
+    result += int(n[i])
+  else:
+    result *= int(n[i])
 
-# print(result)
+print(result)
 
 # 풀이 방법2(by 이코테)
 data = input()
