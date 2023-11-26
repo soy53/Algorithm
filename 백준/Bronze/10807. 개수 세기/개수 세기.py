6c01def4afcd669712e678eb9e450f7bca2e4a16
Soyeon Jeon <116326867+soy53@users.noreply.github.com>
2023-11-26 15:02:33 +0900

    Update 개수 세기.py

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,7 +1,6 @@
+# for문 사용(나의 풀이)
 N = int(input())
-
 n = list(map(int, input().split()))
-
 v = int(input())
 
 count = 0
@@ -9,3 +8,11 @@ for i in n:
   if i == v:
     count += 1
 print(count)
+
+
+-------------------------------------------
+# count() 사용(메모리 적은 풀이)
+N = int(input())
+n = list(map(int,input().split()))
+v = int(input())
+print(n.count(v))
