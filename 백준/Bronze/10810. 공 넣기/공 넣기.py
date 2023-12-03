5a605d65cb13c07dc85ac8585b649a266dc84ff7
Soyeon Jeon <116326867+soy53@users.noreply.github.com>
2023-12-03 16:28:50 +0900

    Update 공 넣기.py

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,3 +1,4 @@
+# join 함수 사용하기
 n, m = map(int, input().split())
 
 basket = [0]*n
@@ -8,3 +9,17 @@ for i in range(m):
     basket[j-1] = c
 
 print(' '.join(map(str, basket)))
+
+# for문 사용하기
+n, m = map(int, input().split())
+
+basket = [0]*n
+
+for i in range(m):
+  a, b, c = map(int, input().split())
+  for j in range(a, b+1):
+    basket[j-1] = c
+
+
+for k in basket:
+  print(k, end=" ")
