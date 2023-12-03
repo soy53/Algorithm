b01286cc5a7e3083daf45b43ea53672be66fc78a
Soyeon Jeon <116326867+soy53@users.noreply.github.com>
2023-12-03 17:43:43 +0900

    Update 과제 안 내신 분．．？.py

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,3 +1,4 @@
+# 내가 푼 방법_리스트 중 0인 부분 찾기
 n_list = [0]*30
 for _ in range(28):
   n = int(input())
@@ -6,3 +7,13 @@ for _ in range(28):
 for i in range(30):
   if n_list[i] == 0:
     print(i+1)
+
+# 다른 사람이 푼 방법_remove
+x=list(map(int,range(1,31)))
+for i in range(28):
+    x.remove(int(input()))
+for j in x:
+    print(j)
+
+# 길이도 시간도 가장 짧은 코드_*, ^
+print(*{*map(int,open(0))}^{*range(1,31)})
