68ef221b47469a5b41137e9bb5dca7d0faca0557
Soyeon Jeon <116326867+soy53@users.noreply.github.com>
2024-01-07 15:03:32 +0900

    Update 평균.py

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,11 +1,17 @@
+# 나의 풀이
 n = int(input())
  
 n_list = list(map(int, input().split()))
 m = max(n_list)
-new_mean = 0
+new_sum = 0
  
 for i in n_list:
   new_score = i/m*100
-  new_mean += new_score
+  new_sum += new_score
  
-print(new_mean/len(n_list))
+print(new_sum/len(n_list))
+
+# 더 짧고 간단한 풀이
+n = int(input())
+n_list = list(map(int,input().split()))
+print(sum(n_list)/max(n_list)*100/n)
