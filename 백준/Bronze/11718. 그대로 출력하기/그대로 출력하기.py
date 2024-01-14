4195aac09b706773dcd59a61a93c4127e2b3fe70
Soyeon Jeon <116326867+soy53@users.noreply.github.com>
2024-01-14 17:39:58 +0900

    Update 그대로 출력하기.py

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -6,5 +6,13 @@ while True:
     break
 
 # for문 이용
-for line in input():
+import sys
+for line in sys.stdin:
     print(line, end="")
+
+# read() 이용
+import sys
+print(sys.stdin.read())
+
+# open(0) 이용
+print(open(0).read()) # open(0).read() == sys.stdin.read()
